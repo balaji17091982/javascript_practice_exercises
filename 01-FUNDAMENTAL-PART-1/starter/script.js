afe589ec40dc4a325cb65b98a997a2b5ad996f6d
Balaji Venkatachalam <balaji.v@thotakaa.com>
2021-11-13 15:37:00 +0530

    variable naming conventions

--- 01-FUNDAMENTAL-PART-1/starter/script.js
+++ 01-FUNDAMENTAL-PART-1/starter/script.js
@@ -1,3 +1,36 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN');
-console.log(40 + 8 + 23 - 10);
+// camelCase for variables with multiple words 
+let firstName = "Venkatachalam";
+console.log(firstName);
+
+// small case for variables with single word
+let age = 36;
+// variables cannot start with a number
+// let 3hours = 3;
+
+//variable names can only contain letters, numbers, umderscores and the dollar sign
+
+// let a&b='5';
+let c$23 = 6
+console.log(c$23);
+
+//variable names cannot be keywords/reserved keywords
+// let new=5;
+// console.log(new);
+// let function=7;
+
+// 'name' is a reserved keyword but is allowed to use. In some cases it will also create problems.
+// let name='Balaji';
+
+//Convention:Variable names hould not start with an upper case letter.
+// let Name='Balaji'
+
+//Convention: For constants we can use all capital case letters
+let PI = 3.1415;
+
+// Convention: Keep the name of the variables as description as you can, so that when u read the code u know what it does.
+// Recommended:
+let myFirstRole = 'C_Developer';
+let myCurrentRole = 'Python_Developer';
+// Not Recommended:
+let role1 = 'C_Developer';
+let role2 = 'Python_Developer';
